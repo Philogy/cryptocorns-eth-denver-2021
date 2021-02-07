cf7c9815d42995e85a460e6cd8120c8ce047896c
MrClottom <philippe@dumo.net>
2021-02-07 14:13:25 +0100

    Create positive rebalance

--- smart-contracts/contracts/debug/Debugger.sol
+++ smart-contracts/contracts/debug/Debugger.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
+import "../interfaces/ICToken.sol";
+
 interface ILeverToken {
     function getEquity() external returns(uint256, uint256);
 }
 
 contract Debugger {
     event EquityFetch(uint256 positiveEquity, uint256 negativeEquity);
+    event UnderlyingBal(uint256 bal);
 
     function getEquity(ILeverToken token) external {
         (uint256 positiveEquity, uint256 negativeEquity) = token.getEquity();
 
         emit EquityFetch(positiveEquity, negativeEquity);
+    }
+
+    function getUnderlyingBalance(ICToken token, address account) external {
+        uint256 underyling = token.balanceOfUnderlying(account);
+        emit UnderlyingBal(underyling);
     }
 }
--- smart-contracts/test/EthDaiLong.test.js
+++ smart-contracts/test/EthDaiLong.test.js
@@ -1,14 +1,5 @@
 const { contract, accounts } = require('@openzeppelin/test-environment')
-const { expectEvent } = require('@openzeppelin/test-helpers')
-const {
-  ether,
-  trackBalance,
-  ZERO,
-  expectEqualWithinError,
-  bnPerc,
-  getDetAddr
-} = require('./utils/general')
-const { takeControlOfOracle } = require('./utils/compound')
+const { ether } = require('./utils/general')
 
 const [deployer, user1] = accounts
 
@@ -16,71 +7,30 @@ const [deployer, user1] = accounts
 const { BN } = require('bn.js')
 const chai = require('chai')
 chai.use(require('chai-bn')(BN))
-const { expect } = chai
 
 const MalleablePriceOracle = contract.fromArtifact('MalleablePriceOracle')
 const EthDaiLong = contract.fromArtifact('EthDaiLong')
-const ICEth = contract.fromArtifact('ICEth')
-const Debugger = contract.fromArtifact('Debugger')
 
 describe('EthDaiLong', () => {
   before(async () => {
-    this.oracleAddr = getDetAddr(deployer, 0)
-    await takeControlOfOracle(
-      this.oracleAddr,
-      '0xc0da01a04c3f3e0be433606045bb7017a7323e38',
-      '0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b'
-    )
+    this.priceOracle = await MalleablePriceOracle.new({ from: deployer })
   })
   beforeEach(async () => {
-    this.priceOracle = await MalleablePriceOracle.new({ from: deployer })
-    expect(this.priceOracle.address).to.equal(this.oracleAddr)
+    await this.priceOracle.resetPrice('DAI')
+    await this.priceOracle.resetPrice('ETH')
 
-    this.debugger = await Debugger.new()
-    this.ethDaiLong = await EthDaiLong.new(this.priceOracle.address, { from: deployer })
-    this.cEth = await ICEth.at(await this.ethDaiLong.collateralCToken())
+    this.ethDaiLong = await EthDaiLong.new(
+      this.priceOracle.address,
+      '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11',
+      { from: deployer }
+    )
   })
-  it('mints based on deposit', async () => {
-    const leverTokenTracker = await trackBalance(this.ethDaiLong, user1)
-
-    expectEvent(await this.debugger.getEquity(this.ethDaiLong.address), 'EquityFetch', {
-      positiveEquity: ZERO,
-      negativeEquity: ZERO
-    })
-
-    const firstTimeAmount = ether('2')
-    await this.ethDaiLong.mint({ from: user1, value: firstTimeAmount })
-
-    let receipt = await this.debugger.getEquity(this.ethDaiLong.address)
-    const startEquity = receipt.logs[0].args.positiveEquity
-    expectEqualWithinError(firstTimeAmount, startEquity, '10', 'A')
-    expect(await leverTokenTracker.delta()).to.be.bignumber.equal(firstTimeAmount, 'B')
-
-    const secondAmount = ether('1.2')
-    await this.ethDaiLong.mint({ from: user1, value: secondAmount })
-
-    receipt = await this.debugger.getEquity(this.ethDaiLong.address)
-    const newEquity = receipt.logs[0].args.positiveEquity
-    expectEqualWithinError(secondAmount, newEquity.sub(startEquity), '10', 'C')
-    expectEqualWithinError(await leverTokenTracker.delta(), secondAmount, '10', 'D')
+  it('simple test', async () => {
+    const mintAmount = ether('1')
+    let receipt = await this.ethDaiLong.mint({ from: user1, value: mintAmount })
+    const [{ args: rebalance }] = receipt.logs.filter(({ event }) => event === 'Rebalance')
+    console.log('rebalance.positive: ', rebalance.positive)
+    console.log('rebalance.fromRatio.toString(): ', rebalance.fromRatio.toString())
+    console.log('rebalance.toRatio.toString(): ', rebalance.toRatio.toString())
   })
-  it('detects rebalance', async () => {
-    const inputValue = ether('1')
-    expectEvent(await this.ethDaiLong.mint({ from: user1, value: inputValue }), 'PositiveRebalance')
-    const ethPrice = await this.priceOracle.price('ETH')
-    const daiPrice = await this.priceOracle.price('DAI')
-    const borrowAmount = bnPerc(inputValue.mul(ethPrice).div(daiPrice), '50')
-
-    await this.ethDaiLong.doBorrow(borrowAmount)
-    let receipt = await this.ethDaiLong.rebalance()
-    expectEvent(receipt, 'PositiveRebalance')
-    expectEvent.notEmitted(receipt, 'NegativeRebalance')
-
-    await this.priceOracle.setPrice('ETH', bnPerc(ethPrice, '40'))
-
-    receipt = await this.ethDaiLong.rebalance()
-    expectEvent(receipt, 'NegativeRebalance')
-    expectEvent.notEmitted(receipt, 'PositiveRebalance')
-  })
-  it('does positive rebalance', async () => {})
 })
