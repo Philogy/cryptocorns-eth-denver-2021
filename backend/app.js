4ede033f80efc6227351bb0d9a94b83eea970340
MrClottom <philippe@dumo.net>
2021-02-10 18:22:03 +0100

    Create semi simpl test

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,29 @@
 const path = require('path')
 const express = require('express')
-const history = require('connect-history-api-fallback')
+// const history = require('connect-history-api-fallback')
 
 const app = express()
 
 const port = process.env.PORT || 8081
 const mode = process.env.NODE_ENV === 'development' ? 'dev' : 'prod'
 
-if (mode === 'dev') {
-  const cors = require('cors')
-  app.use(cors())
-}
-//https
-/*else {
-  app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(`https://${req.get('Host')}${req.url}`)
-    }
-    return next()
-  })
-}*/
+console.log('joined:', path.join(__dirname, 'test-folder'))
+console.log('__dirname: ', __dirname)
 
-app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
 
-const staticPath = path.join(__dirname, 'static')
-const staticMiddleware = express.static(staticPath)
-const historyOptions = {
-  disableDotRule: true,
-  verbose: true
-}
-
-app.use(staticMiddleware)
-app.use(history(historyOptions))
-app.use(staticMiddleware)
+// const staticPath = path.join(__dirname, 'static')
+// const staticMiddleware = express.static(staticPath)
+// const historyOptions = {
+//   disableDotRule: true,
+//   verbose: true
+// }
+//
+// app.use(staticMiddleware)
+// app.use(history(historyOptions))
+// app.use(staticMiddleware)
 
 app.listen(port, () =>
   console.log(`start server on port ${port}; mode: ${mode}; node version: ${process.version}`)
