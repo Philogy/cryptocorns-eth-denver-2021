a1a6d2a0844b5c544220f77412cf5c84bfc1236e
Steven <istvn@icloud.com>
2021-02-12 16:39:23 +0000

    "Create" modal

--- frontend/src/views/Manage/Tokens/Token/Overview.vue
+++ frontend/src/views/Manage/Tokens/Token/Overview.vue
@@ -10,7 +10,7 @@
         <h1 class="text-2xl font-normal">{{ token.desc }}</h1>
       </div>
       <div id="redeem-mint-buttons" class="space-x-12">
-        <el-button type="primary" @click="createTokens">Create</el-button>
+        <el-button type="primary" @click="createVisible = true">Create</el-button>
         <el-button type="primary" @click="redeemTokens">Redeem</el-button>
       </div>
     </div>
@@ -41,6 +41,54 @@
         <span class="info">{{ token.collat }}</span>
       </div>
     </div>
+
+    <el-dialog title="Create" :visible.sync="createVisible" top="35vh" width="560px">
+      <el-form
+        :model="createForm"
+        status-icon
+        ref="crateForm"
+        label-width="120px"
+        class="create-form"
+      >
+        <el-form-item label="Price" prop="price">
+          <el-input
+            type="number"
+            autocomplete="off"
+            v-model="createForm.price"
+            required="true"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Amount" prop="amount">
+          <el-input
+            type="number"
+            autocomplete="off"
+            v-model="createForm.amount"
+            required="true"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item label="Leverage">
+          <el-radio-group v-model="radio1">
+            <el-radio-button label="1x"></el-radio-button>
+            <el-radio-button label="2x"></el-radio-button>
+            <el-radio-button label="4x"></el-radio-button>
+            <el-radio-button label="10x"></el-radio-button>
+            <el-radio-button label="50x"></el-radio-button>
+            <el-radio-button label="100x"></el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="Subtotal"> </el-form-item>
+        <el-form-item label="Gas fee"> </el-form-item>
+        <el-form-item label="Total"> </el-form-item>
+
+        <!-- <el-form-item> -->
+          <el-button type="primary" class="submit-btn" @click="submitForm('ruleForm')"
+            >Create</el-button
+          >
+        <!-- </el-form-item> -->
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -53,7 +101,13 @@ export default {
   components: { CoinPair },
   data: () => ({
     debtCToken: null,
-    collatCToken: null
+    collatCToken: null,
+    createVisible: false,
+    createForm: {
+      price: '',
+      amount: ''
+    },
+    radio1: null
   }),
   computed: {
     token() {
@@ -64,17 +118,17 @@ export default {
     }
   },
   methods: {
-    createTokens() {
-      this.$message({
-        type: 'warning',
-        message: 'No create method implemented yet'
-      })
-    },
     redeemTokens() {
       this.$message({
         type: 'warning',
         message: 'No redeem method implemented yet'
       })
+    },
+    submitForm(formName) {
+      if (formName === 'createForm') {
+        alert('submit!')
+        this.createVisible = false
+      }
     }
   },
   async mounted() {
@@ -121,5 +175,36 @@ export default {
 
 .stat-display > span.info {
   @apply text-white;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+.submit-btn {
+  width: 100%;
+}
+.el-dialog__body {
+  background-color: #1f2025;
+}
+.el-dialog__header {
+  background-color: #1f2025;
+}
+.el-dialog__title {
+  color: white;
+}
+.el-radio-button__inner {
+  background-color: #4c4b58;
+  color: white;
+  border: 1px solid #1f2025;
+}
+.el-radio-button:first-child .el-radio-button__inner {
+  border-left: 1px solid #1f2025;
 }
 </style>
