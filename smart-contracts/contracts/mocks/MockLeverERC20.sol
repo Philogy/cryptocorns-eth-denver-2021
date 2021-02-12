0e38c2d5081e9b2bd325c1ab0956f6b2b3e0edc8
MrClottom <philippe@dumo.net>
2021-02-12 14:39:36 +0100

    Deploy additional tokens

--- smart-contracts/contracts/mocks/MockLeverERC20.sol
+++ smart-contracts/contracts/mocks/MockLeverERC20.sol
@@ -4,17 +4,18 @@ pragma solidity ^0.7.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "../interfaces/IEthERC20Long.sol";
+import "../interfaces/IERC20Long.sol";
 import "../test/BasicPriceOracle.sol";
 
-contract MockLeverToken is ERC20, IEthERC20Long {
+contract MockLeverERC20 is ERC20, IERC20Long {
     using SafeMath for uint256;
 
     uint256 public constant THRESHHOLD = 200;
     uint256 public constant SCALE = 1000000;
     uint256 public immutable targetRatio;
 
-    IERC20 public debtToken;
+    IERC20 public collatToken;
+    address public debtToken;
     BasicPriceOracle public oracle;
 
     uint256 internal _underlyingCollat;
@@ -23,29 +24,33 @@ contract MockLeverToken is ERC20, IEthERC20Long {
     constructor(
         string memory name_,
         string memory symbol_,
-        IERC20 debtToken_,
+        address debtToken_,
+        IERC20 collatToken_,
         BasicPriceOracle oracle_,
         uint256 targetRatio_
     )
         ERC20(name_, symbol_)
     {
         debtToken = debtToken_;
+        collatToken = collatToken_;
         oracle = oracle_;
         require(targetRatio_ <= SCALE, "Invalid ratio");
         targetRatio = targetRatio_;
     }
 
-    function mint() external override payable {
+    function mint(uint256 amount) external override {
+        collatToken.transferFrom(msg.sender, address(this), amount);
+
         if (totalSupply() == 0) {
-            _underlyingCollat = _underlyingCollat.add(msg.value);
+            _underlyingCollat = _underlyingCollat.add(amount);
             emit Rebalance(_rebalance(), 0, _getRatio());
-            _mint(msg.sender, msg.value);
+            _mint(msg.sender, amount);
             _emitEquity();
             return;
         }
 
         uint256 beforeRatio = _getRatio();
-        _underlyingCollat = _underlyingCollat.add(msg.value);
+        _underlyingCollat = _underlyingCollat.add(amount);
 
         if (_withinThreshhold()) {
             return;
@@ -73,14 +78,14 @@ contract MockLeverToken is ERC20, IEthERC20Long {
         _redeemTo(msg.sender, amount);
     }
 
-    function redeemTo(address payable recipient, uint256 amount)
+    function redeemTo(address recipient, uint256 amount)
         external
         override
     {
         _redeemTo(recipient, amount);
     }
 
-    function _redeemTo(address payable recipient, uint256 amount) internal {
+    function _redeemTo(address recipient, uint256 amount) internal {
         require(amount > 0, "Can't redeem nothing");
         require(amount >= balanceOf(msg.sender), "Insufficient balance");
 
@@ -89,14 +94,14 @@ contract MockLeverToken is ERC20, IEthERC20Long {
         uint256 debtShare = _underlyingDebt.mul(amount).div(totalSupply());
         uint256 swappedCollat = _getDebtAsCollat(debtShare);
 
-        _underlyingDebt = _underlyingDebt.sub(debtShare, "A");
-        _underlyingCollat = _underlyingCollat.sub(swappedCollat, "B");
+        _underlyingDebt = _underlyingDebt.sub(debtShare);
+        _underlyingCollat = _underlyingCollat.sub(swappedCollat);
 
-        uint256 equity = _underlyingCollat.sub(_getDebtAsCollat(_underlyingDebt), "C");
+        uint256 equity = _underlyingCollat.sub(_getDebtAsCollat(_underlyingDebt));
         uint256 equityShare = equity.mul(amount).div(totalSupply());
 
-        _underlyingCollat = _underlyingCollat.sub(equity, "D");
-        recipient.transfer(equityShare);
+        _underlyingCollat = _underlyingCollat.sub(equity);
+        collatToken.transfer(recipient, equityShare);
         _burn(msg.sender, amount);
 
         _emitEquity();
@@ -108,12 +113,12 @@ contract MockLeverToken is ERC20, IEthERC20Long {
             block.timestamp
         ));
         uint256 randomWitdth = 2000;
-        return value.sub(randomWitdth, "E").add(uint256(weakSeed) % (randomWitdth * 2));
+        return value.sub(randomWitdth).add(uint256(weakSeed) % (randomWitdth * 2));
     }
 
     function _getRatio() internal view returns(uint256) {
-        uint256 debtUsd = oracle.prices(address(debtToken)).mul(_underlyingDebt);
-        uint256 collatUsd = oracle.prices(address(0)).mul(_underlyingCollat);
+        uint256 debtUsd = _debtPrice().mul(_underlyingDebt);
+        uint256 collatUsd = _collatPrice().mul(_underlyingCollat);
         return debtUsd.mul(SCALE).div(collatUsd);
     }
 
@@ -122,40 +127,35 @@ contract MockLeverToken is ERC20, IEthERC20Long {
         assert(!_withinThreshhold());
 
 
-        uint256 debtUsd = oracle.prices(address(debtToken)).mul(_underlyingDebt);
-        uint256 collatUsd = oracle.prices(address(0)).mul(_underlyingCollat);
+        uint256 debtUsd = _debtPrice().mul(_underlyingDebt);
+        uint256 collatUsd = _collatPrice().mul(_underlyingCollat);
 
         if (currentRatio < targetRatio) {
             uint256 rebalanceValueUsd =
-                targetRatio.mul(collatUsd).sub(debtUsd.mul(SCALE), "F").div(
-                    SCALE.sub(targetRatio, "G")
+                targetRatio.mul(collatUsd).sub(debtUsd.mul(SCALE)).div(
+                    SCALE.sub(targetRatio)
                 );
-            uint256 collatBorrow = rebalanceValueUsd.div(oracle.prices(address(0)));
-            uint256 debtBorrow = rebalanceValueUsd.div(oracle.prices(address(debtToken)));
+            uint256 collatBorrow = rebalanceValueUsd.div(_collatPrice());
+            uint256 debtBorrow = rebalanceValueUsd.div(_debtPrice());
             _underlyingDebt = _underlyingDebt.add(debtBorrow);
             _underlyingCollat = _underlyingCollat.add(collatBorrow);
             return true;
         } else {
             uint256 rebalanceValueUsd =
-                debtUsd.mul(SCALE).sub(targetRatio.mul(collatUsd), "H").div(
-                    SCALE.sub(targetRatio, "I")
+                debtUsd.mul(SCALE).sub(targetRatio.mul(collatUsd)).div(
+                    SCALE.sub(targetRatio)
                 );
-            uint256 debtBorrow = rebalanceValueUsd.div(oracle.prices(address(debtToken)));
-            uint256 collatWithdraw = rebalanceValueUsd.div(oracle.prices(address(0)));
+            uint256 debtBorrow = rebalanceValueUsd.div(_debtPrice());
+            uint256 collatWithdraw = rebalanceValueUsd.div(_collatPrice());
 
-            _underlyingDebt = _underlyingDebt.sub(debtBorrow, "J");
-            _underlyingCollat = _underlyingCollat.sub(collatWithdraw, "K");
+            _underlyingDebt = _underlyingDebt.sub(debtBorrow);
+            _underlyingCollat = _underlyingCollat.sub(collatWithdraw);
             return false;
         }
     }
 
     function _getDebtAsCollat(uint256 debt) internal view returns(uint256) {
-        return debt.mul(
-            oracle.prices(address(debtToken))
-        ).div(
-            oracle.prices(address(0))
-        );
-
+        return debt.mul(_debtPrice()).div(_collatPrice());
     }
 
     function _emitEquity() internal {
@@ -174,6 +174,13 @@ contract MockLeverToken is ERC20, IEthERC20Long {
             ? (currentRatio - targetRatio)
             : (targetRatio - currentRatio);
         return diff <= THRESHHOLD;
+    }
 
+    function _collatPrice() internal view returns(uint256) {
+        return oracle.prices(address(collatToken));
+    }
+
+    function _debtPrice() internal view returns(uint256) {
+        return oracle.prices(debtToken);
     }
 }
